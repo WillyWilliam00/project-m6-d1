57b2c19ca2d21883a6f435db3a95d47388058646
WillyWilliam00 <costawilliam98@gmail.com>
2023-11-23 07:58:01 +0100

    up

--- src/authorRouter.js
+++ src/authorRouter.js
@@ -3,27 +3,21 @@ import { User } from "../schemas/author.js"
 
 const authorsRouter = express.Router()
 
-
-authorsRouter.get("/test", (req, res)=>{
-
-    res.json({message: "authorsRouter working!"})
-})
-
 authorsRouter.get("/",  async (req, res)=>{
 
-    const users = await User.find({});
+   const users = await User.find({});
    res.json(users)
 })
 
 authorsRouter.get("/:id",  async (req, res)=>{
 
-const {id} = req.params
-
    try{
-   const user = await User.findById({id});
-   res.json(user)}
-   catch{
-    console.log("error")
+   const {id} = req.params
+   const user = await User.findById(id);
+   res.status(200).send(user)
+   }
+   catch(error){
+    console.log(error)
     res.status(404).send()
    }  
 })
@@ -34,10 +28,35 @@ authorsRouter.post("/", async (req, res) => {
     const newUser = new User(req.body)
     await newUser.save();
     res.status(201).send(newUser)} 
-    catch{
+    catch(error){
         console.log(error)
+        res.status(404).send()
+    }
+})
+authorsRouter.put("/:id", async (req, res) => {
+
+    try{
+        const {id} = req.params
+        const userUp = await User.findByIdAndUpdate(id, req.body, {new: true})
+        res.status(201).send(userUp)
+    }
+    catch(error){
+       console.log(error)
+       res.status(404).send()
     }
 })
 
+ authorsRouter.delete("/:id", async (req, res) => {
+
+     try{
+         const {id} = req.params
+         const user = await User.findByIdAndDelete(id)
+         res.status(201).send(user)
+     }
+     catch(error){
+        console.log(error)
+        res.status(404).send()
+     }
+ })
 
 export default authorsRouter
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import authorsRouter from "./authorRouter.js";
 import mongoose from "mongoose";
+import cors from "cors";
 
 const server = express()
 const port = 3030   
 
 server.use(express.json())
+server.use(cors())
 server.use("/authors", authorsRouter)
 
 mongoose.connect(process.env.MONGO_URL)
@@ -15,7 +17,7 @@ mongoose.connect(process.env.MONGO_URL)
         console.log("Server listening at port: ", port);
       }); 
 })
-.catch(() => {
-    console.log("Errore nella connessione!")
+.catch((error) => {
+    console.log(error)
 })
 
